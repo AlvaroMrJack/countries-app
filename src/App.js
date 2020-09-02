24a8c8fbe53c6fd7e3da221f579da639c585c2c9
AlvaroMrJack <alvaro.12tobar@gmail.com>
2020-09-02 01:50:23 -0400

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,113 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-function App() {
+//  APPBAR
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import PublicIcon from '@material-ui/icons/Public';
+
+//  CONTAINER
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+
+//  GRID
+//import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    flexGrow: 1,
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.primary,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function SimpleContainer() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <React.Fragment>
+
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Listado de paises
+          </Typography>
+          <Button
+            variant="outlined"
+            color="inherit"
+            className={classes.button}
+            endIcon={<PublicIcon>PublicIcon</PublicIcon>}
+          >
+            Últimos sismos
+          </Button>
+        </Toolbar>
+      </AppBar>
+
+      <CssBaseline />
+        <Container fixed>
+          <Grid container spacing={3}>
+            
+            <Grid item xs={12}>
+
+              <Typography variant="h3" gutterBottom>
+                Listado de paises 
+              </Typography>
+              <div className={classes.root}>
+                <Accordion>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                  >
+                    <Typography className={classes.heading}>Accordion 1</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                      sit amet blandit leo lobortis eget.
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+              </div>
+
+            </Grid>
+
+          </Grid>
+
+        </Container>
+    </React.Fragment>
+    
   );
 }
-
-export default App;
